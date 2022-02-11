5650350993f7c63d21c8f2ebfc3e24a43dedfbac
Frank Orellana <lando.orellana@gmail.com>
2022-02-10 23:34:52 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 pnpm build
+git add .
+git commit -m "deploy"
+
 git switch gh-pages
 git checkout $1 -- dist/
 rm -rf docs/
@@ -7,3 +10,5 @@ mv dist/* docs/
 git add .
 git commit -m "deploy"
 git push
+
+git switch $1
